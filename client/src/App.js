e482839784c891c37e1567aea50235e2a49b66a8
nidhi2793 <MY_NAME@example.com>
2023-09-15 16:26:57 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,18 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Home from "./pages/Home";
 import AddEdit from "./pages/AddEdit";
+import Header from "./pages/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <ToastContainer position="top-right" />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/addExpense" element={<AddEdit />}></Route>
-          <Route path="/update/:id" element={<AddEdit />}></Route>
-        </Routes>
-      </div>
+      <Header />
+      <ToastContainer position="top-right" />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/addExpense" element={<AddEdit />}></Route>
+        <Route path="/update/:id" element={<AddEdit />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
